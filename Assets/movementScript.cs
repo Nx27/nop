727d88fe87df5c3375242bf8c594034853d34ff1
Nora_de_Greeuw <37214@ma-web.nl>
2023-11-23 12:59:02 +0100

    psuh

--- Assets/movementScript.cs
+++ Assets/movementScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,7 @@ public class movementScript : MonoBehaviour
 {
     private float maxMovementSpeed = 10.0f;
     private float lerpSpeed = 0.5f;
-    private float movement = 1;
+    private float movement = 0.1f;
     Rigidbody2D body;
 
     public float Movement { get => movement; set => movement = value; }
@@ -19,23 +20,37 @@ public class movementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetKey(KeyCode.A) ? -1 : Input.GetKey(KeyCode.D) ? 1 : 0;
+        float vertical = Input.GetKey(KeyCode.S) ? -1 : Input.GetKey(KeyCode.W) ? 1 : 0;
 
-        Vector2 vector2 = new Vector2(horizontal, vertical).normalized;
-        Vector2 UP = new Vector2(10, 0);
+        Vector2 vector2 = new Vector2(horizontal, vertical);
         Vector2 generalMovement = vector2 * movement;
-        body.AddForce(generalMovement);
-        
-        if (horizontal < maxMovementSpeed)
+        //Debug.Log(generalMovement);
+        body.AddForce(generalMovement, ForceMode2D.Impulse);
+
+        if (body.velocity.x > maxMovementSpeed)
         {
-            horizontal = maxMovementSpeed;
-        }
-        if (vertical < maxMovementSpeed)
-        {
-            vertical = maxMovementSpeed;
+            body.velocity = new Vector2(maxMovementSpeed, body.velocity.y);
         }
 
-        
+        if (body.velocity.y > maxMovementSpeed)
+        {
+            body.velocity = new Vector2(body.velocity.x, maxMovementSpeed);
+        }
+
+        if (horizontal == 0)
+        {
+            body.velocity = new Vector2(0, body.velocity.y);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            body.velocity = new Vector2(body.velocity.x, 0);
+        }
     }
+    //private void RemoveAllForces()
+    //{
+    //    gameObject.SetActive(false);
+    //    gameObject.SetActive(true);
+    //}
 }
