2c44df64e923f2f993800da983e9fc83637dfba2
DESKTOP-L4V3QNJ\jdsuu <238632@student.scalda.nl>
2024-01-12 13:12:08 +0100

    mijn brein kan dit nu niet hendelen, ga ik later mee door

--- Assets/Jeroen/Player/Scripts/playerController.cs
+++ Assets/Jeroen/Player/Scripts/playerController.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     private float speed = 300f;
-    private float jumpForce;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float jumpForce;
 
     // Start is called before the first frame update
     void Start()
@@ -21,32 +21,15 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        jumpForce = jump();
+
     }
+
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, jumpForce);
-    }
-
-
-    //Jump is janky en broken
-    //TODO: Fix deze bullshit
-    float jump()
-    {
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (Input.GetKey(KeyCode.W))
         {
-            return 10f;
+            rb.velocity = (new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime,jumpForce));
         }
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f && IsGrounded())
-        {
-            return rb.velocity.y * 0.5f;
-        }
-
-        return rb.velocity.y;
-    }
-
-    bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        rb.velocity = (new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f));
     }
 }
