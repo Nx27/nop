c21332a3ec18e116d237e22899457a83e8531dba
jdsuu <j.d.suurmon@icloud.com>
2024-01-13 15:22:48 +0100

    Dubbele UnityEngine import weggehaald in kevins file

--- Assets/Kevin/Kevin/scripts/Control/PlayerControllerKev.cs
+++ Assets/Kevin/Kevin/scripts/Control/PlayerControllerKev.cs
@@ -2,7 +2,6 @@ namespace Kevin.Control {
     using UnityEngine;    
     using System.Collections;
     using System.Collections.Generic;
-    using UnityEngine;
     
     
     public class PlayerControllerKev : MonoBehaviour {
