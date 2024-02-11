01b0ecd490b162e6177e41d8d3a4baa41df72da9
Nxbi <37214@ma-web.nl>
2024-02-11 20:10:34 +0100

    ehm

---
+++ Assets/Scenes/Nora/UI/SettingUIHandler.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+public class SettingUIHandler : MonoBehaviour
+{
+        //[SerializeField]
+        //private TextMeshPro textMeshPro;
+
+    [SerializeField] TMP_Text text;
+
+        void Start()
+        {
+            //textMeshPro.text = "Some text";
+        text.text = "why";
+        }
+
+
+    void Update()
+    {
+        
+    }
+}
