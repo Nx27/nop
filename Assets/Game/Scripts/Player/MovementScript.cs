e47ad313fd3fbb87dc8a3e64d649983eac89cf8c
Nxbi <37214@ma-web.nl>
2024-02-21 23:59:41 +0100

    trustmeitworks

--- Assets/Game/Scripts/Player/MovementScript.cs
+++ Assets/Game/Scripts/Player/MovementScript.cs
@@ -1,49 +1,81 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using UnityEngine;
 public class MovementScript : MonoBehaviour
 {
     public double _MoveSpeed { get => MoveSpeed; set => MoveSpeed = value; }
+    public bool _CanJump { get => CanJump; set => CanJump = value; }
+    public bool _CanJumpTwice { get => CanJumpTwice; set => CanJumpTwice = value; }
 
+    #region Code
 
+    #region Fields
 
     private Rigidbody2D RB;
-    private Vector2 Movement;
-    private double MoveSpeed = 1;
-    private GameObject GroundCheck, HeadCheck, FrontCheck;
-    private int GroundLayer;
+    private double MoveSpeed = 10;
+    private bool CanJump;
+    private bool CanJumpTwice;
 
+    #endregion
 
-    RaycastHit hit;
+    #region Function
     void Start()
     {
-        GroundCheck = GameObject.Find("GroundCheck");
-        HeadCheck = GameObject.Find("HeadCheck");
-        FrontCheck = GameObject.Find("FrontCheck");
         RB = GetComponent<Rigidbody2D>();
-        GroundLayer = (1 << 6);
     }
 
     void Update()
     {
-        float Horizontal = Input.GetAxis("Horizontal");
-        double HorizontalMovement = Horizontal * 0.1 * MoveSpeed;
-        Movement = new((float)HorizontalMovement, 0);
-        transform.Translate(Movement, Space.Self);
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector2 Left = new((float)-MoveSpeed, RB.velocity.y);
+            RB.AddForce(Left);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector2 Right = new((float)MoveSpeed, RB.velocity.y);
+            RB.AddForce(Right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && CanJumpTwice)
+        {
+            Debug.Log("jumptwice");
+            CanJumpTwice = false;
+            Vector2 Jump = new(0, 10);  
+            RB.AddForce(Jump, ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && CanJump)
+        {
+            Debug.Log("jump");
+            Vector2 Jump = new(0, 10);
+            RB.AddForce(Jump, ForceMode2D.Impulse);
+            CanJump = false;
+            CanJumpTwice = true;
+        }
+
+        if (RB.velocity.magnitude > 0)
+        {
+            Vector2 Oppose = -RB.velocity;
+            RB.AddForce(Oppose);
+        }
 
     }
 
 
-    private void OnTriggerStay2D(UnityEngine.Collider2D collision)
+    private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
     {
         GameObject other = collision.gameObject;
         if (other.layer == LayerMask.NameToLayer("Ground"))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Debug.Log("jump");
-            }
+            CanJump = true;
+            CanJumpTwice = false;
         }
     }
 }
+#endregion
+
+#endregion
