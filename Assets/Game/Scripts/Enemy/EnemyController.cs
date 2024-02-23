3b0f354ef673da20082853bc31d27bc80de5b2e7
Nxbi <37214@ma-web.nl>
2024-02-23 19:19:44 +0100

    Update EnemyController.cs

--- Assets/Game/Scripts/Enemy/EnemyController.cs
+++ Assets/Game/Scripts/Enemy/EnemyController.cs
@@ -6,26 +6,30 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     #region Fields
-    [SerializeField] Rigidbody2D body;
-    [SerializeField] Rigidbody2D target;
-    [SerializeField] OnCollisionEnter2D onCollisionEnter;
-
     [SerializeField] float maxHealth = 10;
+    [SerializeField] float speed = 1;
+
     private float health;
-    [SerializeField] float speed;
+
     #endregion
 
     #region Code
-    // Start is called before the first frame update
+
     void Start()
     {
         body = gameObject.AddComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       
+
+
     }
 
     #endregion
