eb8c5903366b0419e81e4f689da1218ae4e5fa3f
kevin <kevinhamelink05@gmail.com>
2024-02-23 11:59:24 +0100

    Lichte progressie

--- Assets/Game/Scripts/Enemy/EnemyController.cs
+++ Assets/Game/Scripts/Enemy/EnemyController.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-
+    #region Fields
     [SerializeField] Rigidbody2D body;
     [SerializeField] Rigidbody2D target;
     [SerializeField] OnCollisionEnter2D onCollisionEnter;
 
+    [SerializeField] float maxHealth = 10;
+    private float health;
+    [SerializeField] float speed;
+    #endregion
+
+    #region Code
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = gameObject.AddComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -21,4 +27,7 @@ public class EnemyController : MonoBehaviour
     {
         
     }
+
+    #endregion
+
 }
